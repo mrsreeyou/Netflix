8d18029fa775cc9a405f8db83cadd33c090acc88
Sreekesh Narayanan p p <sreekeshnarayanpp04@gmail.com>
2025-01-07 14:58:26 +0530

    Update constants.jsx

--- src/constans/constants.jsx
+++ src/constans/constants.jsx
@@ -1,5 +1,5 @@
 export const baseUrl='https://api.themoviedb.org/3'
 
-export const API_KEY ='878ac9cadba2ddfd2491a3de3d4b71d4' ;
+export const API_KEY ='your-Api-key' ;
 
 export const imageUrl='https://image.tmdb.org/t/p/original'
