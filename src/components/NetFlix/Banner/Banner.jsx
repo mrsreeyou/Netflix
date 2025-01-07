78f84dcadc252ba3b1a924bec1102f9d1a758f59
unknown <sreekesh>
2025-01-07 14:51:12 +0530

    constant-ignore

--- src/components/NetFlix/Banner/Banner.jsx
+++ src/components/NetFlix/Banner/Banner.jsx
@@ -8,7 +8,7 @@ function Banner() {
 
   useEffect(()=>{
     axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{   //trending
-      console.log(response.data.results[4]);
+      console.log(response.data.results[5]);
       setMovie(response.data.results[0])
     })
   },[])
